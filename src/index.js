269d7b85648e8beb225008f576ad42155409bb47
russell Hofvendahl <rhofvendahl@gmail.com>
2021-03-02 15:06:47 -0800

    refactor constants

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+
+// Renders background.
 function Background(props) {
   return (
     <div className="stripe">
@@ -46,13 +48,6 @@ function Box(props) {
         translate={{x: (props.coordinates.x+props.dimensions.x/2), y: -(props.coordinates.y+props.dimensions.y), z: (props.coordinates.z+props.dimensions.z/2)}}
         rotate={{x: 90, y: 0, z: 0}}
       />
-      {/* <Face
-        color={props.pallete.dark}
-        width={props.dimensions.x}
-        height={props.dimensions.z}
-        translate={{x: (props.coordinates.x+props.dimensions.x/2), y: -(props.coordinates.y), z: (props.coordinates.z+props.dimensions.z/2)}}
-        rotate={{x: 90, y: 0, z: 0}}
-      /> */}
 
       {/* orthogonal to z axis (z points out at screen) */}
       <Face
@@ -62,67 +57,52 @@ function Box(props) {
         translate={{x: (props.coordinates.x+props.dimensions.x/2), y: -(props.coordinates.y+props.dimensions.y/2), z: (props.coordinates.z+props.dimensions.z)}}
         rotate={{x: 0, y: 0, z: 0}}
       />
-      {/* <Face
-        color={props.pallete.medium}
-        width={props.dimensions.x}
-        height={props.dimensions.y}
-        translate={{x: (props.coordinates.x+props.dimensions.x/2), y: -(props.coordinates.y+props.dimensions.y/2), z: (props.coordinates.z)}}
-        rotate={{x: 0, y: 0, z: 0}}
-      /> */}
-
-      {/* orthogonal to x axis (x points to the right) */}
-      {/* <Face
-        color={props.pallete.light}
-        width={props.dimensions.z}
-        height={props.dimensions.y}
-        translate={{x: (props.coordinates.x+props.dimensions.x), y: -(props.coordinates.y+props.dimensions.y/2), z: (props.coordinates.z+props.dimensions.z/2)}}
-        rotate={{x: 0, y: 90, z: 0}}
-      /> */}
-      {/* <Face
-        color={props.pallete.dark}
-        width={props.dimensions.z}
-        height={props.dimensions.y}
-        translate={{x: (props.coordinates.x), y: -(props.coordinates.y+props.dimensions.y/2), z: (props.coordinates.z+props.dimensions.z/2)}}
-        rotate={{x: 0, y: 90, z: 0}}
-      /> */}
     </div>
   )
 }
+
+const PALLETES = {
+  WATER: {
+    light: "#4081f2",
+    medium: "#346dc7",
+    dark: "#275799",
+  },
+  SAND: {
+    light: "#FFF089",
+    medium: "#C1B367",
+    dark: "#817847",
+  },
+  FOLIAGE: {
+    light: "#2aa330",
+    medium: "#1f8c28",
+    dark: "#106e1f",
+  },
+  ROCK: {
+    light: "#BEBEBE",
+    medium: "#8E8E8E",
+    dark: "#606060",
+  },
+  SNOW: {
+    light: "#FFFFFF",
+    medium: "#BEBEBE",
+    dark: "#7F7F7F",
+  },
+};
 
 // Responsible for rendering landscape.
 class Scape extends React.Component {
   getPallete(height) {
     const snowLine = 3;
-    if (height <= .2*snowLine) { // water
-      return {
-        light: "#4081f2",
-        medium: "#346dc7",
-        dark: "#275799",
-      }
-    } else if (height <= .3*snowLine) { // sand
-      return {
-        light: "#FFF089",
-        medium: "#C1B367",
-        dark: "#817847",
-      }
-    } else if (height <= .6*snowLine) { // foliage
-      return {
-        light: "#2aa330",
-        medium: "#1f8c28",
-        dark: "#106e1f",
-      }
-    } else if (height <= snowLine) { // rock
-      return {
-        light: "#BEBEBE",
-        medium: "#8E8E8E",
-        dark: "#606060",
-      }
-    } else { // snow
-      return {
-        light: "#FFFFFF",
-        medium: "#BEBEBE",
-        dark: "#7F7F7F",
-      }
+    if (height <= .2*snowLine) {
+      return PALLETES.WATER
+    } else if (height <= .3*snowLine) {
+      return PALLETES.SAND
+    } else if (height <= .6*snowLine) {
+      return PALLETES.FOLIAGE
+    } else if (height <= snowLine) {
+      return PALLETES.ROCK
+    } else {
+      return PALLETES.SNOW
     };
   }
 
@@ -183,13 +163,18 @@ class Scape extends React.Component {
   }
 }
 
+const INIT = {
+  X_CELLS: 20,
+  Z_CELLS: 20,
+  CELL_SIZE: 20,
+  CLICK_MEMORY: 20,
+  UPDATE_INTERVAL: 200,
+}
+
 // Responsible for landscape interactivity.
 class ScapeManager extends React.Component {
   constructor(props) {
     super(props);
-    this.xCells = 20;
-    this.zCells = 20;
-    this.cellSize = 20;
     this.state = {
       clickLog: [
         [5, 6, Date.now()],
@@ -197,11 +182,9 @@ class ScapeManager extends React.Component {
         [14, 14, Date.now()+500],
         [16, 4, Date.now()+1000],
       ],
-      map: new Array(this.xCells).fill(0).map(() => new Array(this.zCells).fill(0)),
+      map: new Array(INIT.X_CELLS).fill(0).map(() => new Array(INIT.Z_CELLS).fill(0)),
     };
-    this.clickMemory = 5;
     this.updateTimer = undefined;
-    this.updateInterval = 100;
   }
 
   handleClick(boxName) {
@@ -219,12 +202,12 @@ class ScapeManager extends React.Component {
   // Calculates the desired height for each cell.
   // Each cell is calculated individually, based on its distance from the locations of recent clicks.
   updateMap() {
-    const map = new Array(this.xCells).fill(0).map(() => new Array(this.zCells).fill(0));
+    const map = new Array(INIT.X_CELLS).fill(0).map(() => new Array(INIT.Z_CELLS).fill(0));
     for (let x=0; x<map.length; x++) {
       for (let z=0; z<map[0].length; z++) {
 
         // For each click, add to the current cell's height.
-        for (let i = 0; (i < this.clickMemory) && (i < this.state.clickLog.length); i++) {
+        for (let i = 0; (i < INIT.CLICK_MEMORY) && (i < this.state.clickLog.length); i++) {
           const click = this.state.clickLog[this.state.clickLog.length-1-i];
           const seconds = (Date.now() - click[2])/1000;
           const distance = Math.pow((Math.pow(x-click[0], 2)+Math.pow(z-click[1], 2)), 1/2);
@@ -246,7 +229,7 @@ class ScapeManager extends React.Component {
   componentDidMount() {
     this.updateTimer = setInterval(()=>{
       this.updateMap();
-    }, this.updateInterval);
+    }, INIT.UPDATE_INTERVAL);
   }
 
   componentWillUnmount() {
@@ -256,7 +239,7 @@ class ScapeManager extends React.Component {
   render() {
     return (
       <Scape 
-        cellSize={this.cellSize}
+        cellSize={INIT.CELL_SIZE}
         handleClick={(boxName)=>this.handleClick(boxName)}
         map={this.state.map}
       />
